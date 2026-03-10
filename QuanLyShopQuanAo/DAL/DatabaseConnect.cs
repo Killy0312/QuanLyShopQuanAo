b28e7876d42c5a6091fb8cb64d4027f7a0a9ea32
LENOVO LOQ <LENOVO LOQ@LAPTOP-RLFS7GV5>
2026-03-10 23:46:36 +0700

    # Update Database connect

--- QuanLyShopQuanAo/DAL/DatabaseConnect.cs
+++ QuanLyShopQuanAo/DAL/DatabaseConnect.cs
@@ -1,13 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace QuanLyShopQuanAo.DAL
 {
-    // Test khả năng update github
-    internal class DatabaseConnect
+    public class DatabaseConnect
     {
+        private string connectionString =
+         "Data Source=.;Initial Catalog=QuanLyCuaHangQuanAo;Integrated Security=True";
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
+            }
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public object ExecuteScalar(string query)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                return cmd.ExecuteScalar();
+            }
+        }
     }
 }
